b539557f2522d5070cc04982e4c32387f3ffa8e8
Unknown <dm.gonzalezperez@ugto.mx>
2017-09-18 15:34:06 -0500

    Division
    Corregi la función

--- calculadora.cpp
+++ calculadora.cpp
@@ -24,10 +24,12 @@ int main(){
 		case 4: division(a,b);
 	}
 
-    float division(int a, int b)
+    
+	return 0;
+}
+
+float division(int a, int b)
     {
         cout<<"\n Soy la División = "<< a/b;
     }
-	return 0;
-}
 
